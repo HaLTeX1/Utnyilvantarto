f274b53369bb1af8ffb820d7a8b5eb4c68ce5d1d
HaLTeX1 <pracni@gmail.com>
2024-02-25 12:19:25 +0100

    Rendszam validálás
    - Már kizárólag a magyar publikus rendszámokat fogadja el
    - Variable típuskonverziók
    - Teljes adatfájl megtekintés

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ majd egy táblázatban megjeleníti ezeket az adatokat.
 from prettytable import PrettyTable
 import shortuuid
 import csv
+import re
+
+def valid_rendszam(rendszam):
+    rendszam_regex = re.compile(r'^[A-Z0-9]{2,}(?:-[A-Z]{2,})?(?:-[A-Z0-9]{1,})?$')    # Rendszámok formátuma: AAA-123 ,AA-BB-123
+    return bool(re.match(rendszam_regex, rendszam))
 
 table = PrettyTable()
 km = 0
@@ -18,13 +23,17 @@ uttipus = ["Magánút", "Céges", "Egyéb", "Nem ismert"]
 azonosito = shortuuid.uuid()
 tipus_adatbekeres = input("Kérlek, add meg a gépjármű típusát: ")
 rendszam_bekeres = input("Kérlek, add meg a gépjármű rendszámát: ").upper()
+while not valid_rendszam(rendszam_bekeres):
+    print("Hibás rendszám formátum. Kérlek, adj meg egy érvényes rendszámot!")
+    rendszam_bekeres = input("Kérlek, add meg a gépjármű rendszámát: ").upper()
+
 km_adatbekeres = int(input("Kérlek, add meg a kilóméteróra állását: "))
 uttipus_adatbekeres = input("Kérlek add meg az út tipusát az alábbiak közül: // Magánút, Céges, Egyéb, Nem ismert // => ")
 
 
 tipus = tipus_adatbekeres
 rendszam = rendszam_bekeres
-km = km_adatbekeres + " km"
+km = str(km_adatbekeres) + " km"
 uttipus = uttipus_adatbekeres
 
 
@@ -39,3 +48,12 @@ with open('gepjarmu_adatok.csv', mode='a', newline='') as file:
         writer.writerow(["Azonosító" ,"Típus", "Rendszám", "Óraállás", "Úttípus"])
     # Írja ki az új rekordot
     writer.writerow([azonosito, tipus, rendszam, km, uttipus])
+
+adatmegtek = input("Kívánod megtekinteni az eddigi összes adatot a fájlban? (IGEN / NEM): ").upper()
+if adatmegtek == "IGEN":
+    with open('gepjarmu_adatok.csv', mode='r') as file:
+        reader = csv.reader(file)
+        table.field_names = next(reader)  # Az első sor a fejléc
+        for row in reader:
+            table.add_row(row)
+    print(table)
